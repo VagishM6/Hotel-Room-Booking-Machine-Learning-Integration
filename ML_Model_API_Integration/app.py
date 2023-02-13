660d8bcad30afe3551a97710aacf51286e7297e3
Vagish Mohanakumar <iamvagish6@gmail.com>
2023-02-13 19:14:02 +0530

    changing port number of the host

--- ML_Model_API_Integration/app.py
+++ ML_Model_API_Integration/app.py
@@ -47,4 +47,4 @@ def predict_occupancy(data:RoomOccupancy):
 
 # running the API interface with uvicorn
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+    uvicorn.run(app, host='127.0.0.0', port=8000)
